5667afe9b5b23f984a951c0fbab73738d9ba406d
ojas-sohni <ojas.sohni@gmail.com>
2022-02-07 17:50:06 +0530

    Implemented redirects, nested routing in dashboard and sidebar components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -16,13 +16,17 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
             <Route
               path="/products"
               render={(props) => <Products sortBy="newest" {...props} />}
             />
-            <Route path="/posts" component={Posts} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -3,12 +3,18 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+
+    //this.props.history.push("/products");
+    //Using push we can go back to pevious page.
+
+    this.props.history.replace("/products");
+    //Using replace we cannot go to pevious page, This is often used when a user logs in and goes to a new page, If he click back button we dont want him to go back to login page.
   };
 
   render() {
     return (
       <div>
-        <h1>Product Details - </h1>
+        <h1>Product Details -{this.props.match.params.id} </h1>
         <button onClick={this.handleSave}>Save</button>
       </div>
     );
